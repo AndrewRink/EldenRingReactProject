f0888dde1ac857f19ea9e27cbfa2603c6414356e
Andrew <andrewcrink@gmail.com>
2023-01-24 18:47:27 -0500

    project started, added Home page component

---
+++ src/Components/Home.js
@@ -0,0 +1,11 @@
+
+function Home () {
+    return(
+        <div className = "homepage">
+            <h1>Welcome to the Elden Ring Encyclopedia!</h1>
+            <h2>We have helpful information on almost everything Elden Ring!</h2>
+        </div>
+    )
+}
+
+export default Home
